7eff6cb16a9d78cef918f1ca2f2e583c347646aa
AdamC7313 <AdamC7313@gmail.com>
2022-10-07 14:54:20 -0400

    add list of movies and table dynamics

--- src/Components/MovieList/MovieList.js
+++ src/Components/MovieList/MovieList.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import listOfMovies from './listOfMovies';
 import './MovieList.css';
+
+
+
+function createMovieTable() {
+    const table = []
+    for (let i = 0; i < listOfMovies.length; i++) {
+        table.push(            
+        <tr>          
+            <td>{listOfMovies[i].title}</td>
+            <td>{listOfMovies[i].genre}</td>
+            <td>{listOfMovies[i].watched}</td>
+        </tr>)
+    }
+    return table;
+} 
 
 class MovieList extends React.Component {
     render() {
         return(
             <div className="movie-table">
             <table>
-            <thead>
-                <th>TITLE</th>
-                <th>GENRE</th>
-                <th>WATCHED?</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>test movie 1</td>
-                    <td>Comedy</td>
-                    <td>No</td>
-                </tr>
-            </tbody>
-        </table>
-        </div>
+                <thead>
+                    <th>TITLE</th>
+                    <th>GENRE</th>
+                    <th>WATCHED?</th>
+                </thead>
+                <tbody>
+                    {createMovieTable()}          
+                 </tbody>
+            </table>
+         </div>
         )
     }
 }
---
+++ src/Components/MovieList/listOfMovies.js
@@ -0,0 +1,5 @@
+const listOfMovies = [
+    {title: 'test movie 1', genre: 'comedy', watched: 'no'},
+    {title: 'test movie 2', genre: 'comedy', watched: 'no'}]
+
+export default listOfMovies;
